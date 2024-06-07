e43ced9278c1b0eb44e7dc7c94307646053361d3
decade <20120220@student.hcmus.edu.vn>
2024-06-07 14:12:07 +0700

    dang test phan plug-in, cac phan con lai ok, do conflict nen tao them branch

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   testPathIgnorePatterns: [
-    "./__test__/authen.test.js",
-    "./__test__/novel.test.js",
-    "./__test__/prefs.test.js",
-    "./__test__/supplier.test.js",
-    "./__test__/user.test.js",
+    // "./__test__/authen.test.js",
+    // "./__test__/novel.test.js",
+    // "./__test__/prefs.test.js",
+    // "./__test__/supplier.test.js",
+    // "./__test__/user.test.js",
+    "./__test__/plugin.test.js",
   ],
 };
